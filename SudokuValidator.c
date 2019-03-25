70d94b79a6a99ea6af33947f4de08130d1884b3a
oscreader <oscreader@OSC>
2019-03-25 12:01:13 -0600

    Init ree

--- SudokuValidator.c
+++ SudokuValidator.c
@@ -6,18 +6,21 @@
 #include <unistd.h>
 #include <sys/io.h>
 #include <sys/mman.h>
+#include <pthread.h>
 
 void *validateRows(void *);
 void *validateCols(void *);
 void *validateSubGrid(void *);
+
+int **board;
 
 int main(int argc, char const *argv[])
 {
     unsigned char *f;
     int size = 81;
     // Create and allocate array of integer pointers
+    board = (int**)malloc(9 * sizeof(int *));
     int c;
-    int **board = (int**)malloc(9 * sizeof(int *));
     for (c = 0; c < 9; ++c)
     	board[c] = (int *)malloc(9 * sizeof(int *));
     // Array of threads
@@ -40,5 +43,22 @@ int main(int argc, char const *argv[])
 }
 
 void *validateRows(void *data) {
-	
+	int i, j;
+	// Para que recorra todas las filas
+	for (i = 0; i < 9; j++) {
+		// Reiniciando el array de validacion
+		int valid[9] = {0};
+		// Recorriendo la fila
+		for (j = 0; j < 9; j++) {
+			// Ver que numero hay en esta posicion del array
+			int current = board[i][j];
+			// Si el numero ya habia sido encontrado, que regrese null
+			if (valid[current - 1] == 1){
+				pthread_exit(NULL);
+			// Si el numero no habia sido encontrado, se agrega al array de encontrados
+			} else {
+				valid[current - 1] = 1;
+			}
+		}	
+	}
 }
