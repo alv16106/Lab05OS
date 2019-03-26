67e540ea99dac0fa2a085bf6fb472a2a33fbf48b
oscreader <oscreader@OSC>
2019-03-25 23:38:33 -0600

    Full program

--- SudokuValidator.c
+++ SudokuValidator.c
@@ -4,8 +4,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <sys/io.h>
 #include <sys/mman.h>
+#include <sys/syscall.h>
 #include <pthread.h>
 
 void *validateRows(void *);
@@ -23,6 +26,7 @@ int main(int argc, char const *argv[])
     int c;
     for (c = 0; c < 9; ++c)
     	board[c] = (int *)malloc(9 * sizeof(int *));
+
     // Array of threads
     pthread_t ids[3];
 
@@ -34,12 +38,47 @@ int main(int argc, char const *argv[])
     	for (j = 0; j < 9; j++)
     	{
     		now = (int)f[i*9 + j] - '0';
-    		printf("%d\n", now);
     		board[i][j] = now;
     	}
     }
 
-    return 0;
+    void *dummy;
+    int status[3];
+    pthread_create(&ids[0], NULL, validateSubGrid, dummy);
+    int pid = getpid();
+    pid_t p1;
+    p1 = fork();
+	if (p1 > 0){
+    	printf("Reeee");
+    	pthread_create(&ids[1], NULL, validateCols, dummy);
+    	pthread_join(ids[0], (void *)&status[0]);
+    	pthread_join(ids[1], (void *)&status[1]);
+    	pid_t tid = syscall(SYS_gettid);
+    	printf("%d", tid);
+    	wait(NULL);
+    	pthread_create(&ids[2], NULL, validateRows, dummy);
+    	pthread_join(ids[2], (void *)&status[2]);
+    	int h, flag;
+    	for (h = 0; h < 3; h++)
+    	{
+    		if (!status[h]) {
+    			flag = 1;
+    		}
+    	}
+    	if (flag)
+    	{
+    		printf("Sudoku invalido");
+    	}else {
+    		printf("Sudoku valido");
+    	}
+    	return 0;
+    } else {
+    	printf("%d", 3443);
+    	char buffer[55];
+    	sprintf(buffer, "ps -p %ld -lLf", pid);
+    	printf("%s", buffer);
+    	execlp((char *)&buffer, (char *)&buffer, (char *)NULL);
+    }
 }
 
 void *validateRows(void *data) {
@@ -54,11 +93,61 @@ void *validateRows(void *data) {
 			int current = board[i][j];
 			// Si el numero ya habia sido encontrado, que regrese null
 			if (valid[current - 1] == 1){
-				pthread_exit(NULL);
+				return (void *) 0;
 			// Si el numero no habia sido encontrado, se agrega al array de encontrados
 			} else {
 				valid[current - 1] = 1;
 			}
 		}	
 	}
+	return (void *) 1;
+}
+
+void *validateCols(void *data) {
+	int i, j;
+	// Para que recorra todas las columnas
+	for (i = 0; i < 9; j++) {
+		// Reiniciando el array de validacion
+		int valid[9] = {0};
+		// Recorriendo la fila
+		for (j = 0; j < 9; j++) {
+			// Ver que numero hay en esta posicion del array
+			int current = board[j][i];
+			// Si el numero ya habia sido encontrado, que regrese null
+			if (valid[current - 1] == 1){
+				return (void *) 0;
+			// Si el numero no habia sido encontrado, se agrega al array de encontrados
+			} else {
+				valid[current - 1] = 1;
+			}
+		}
+	}
+	return (void *) 1;
+}
+
+void *validateSubGrid(void *data) {
+	int si, sj, i, j;
+	for (si = 0; si < 9; si+=3)
+	{
+		for (sj = 0; sj < 9; sj+=3)
+		{
+			int valid[9] = {0};
+			for (i = 0; i < 3; i++)
+			{
+				for (j = 0; j < 3; j++)
+				{
+					// Ver que numero hay en esta posicion del array
+					int current = board[si + i][sj + j];
+					// Si el numero ya habia sido encontrado, que regrese null
+					if (valid[current - 1] == 1){
+						return (void *) 0;
+					// Si el numero no habia sido encontrado, se agrega al array de encontrados
+					} else {
+						valid[current - 1] = 1;
+					}
+				}
+			}
+		}
+	}
+	return (void *) 1;
 }
